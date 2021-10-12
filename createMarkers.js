748a8d4d9659c53f76340b10a4130b07d6bbbac9
Pruthvi Shrikaanth <38621789+Blue0Tech@users.noreply.github.com>
2021-10-12 21:05:08 +0100

    Added ordering function

--- createMarkers.js
+++ createMarkers.js
@@ -12,6 +12,10 @@ AFRAME.registerComponent('create-markers',{
             });
             marker.setAttribute('marker-handler',{});
             mainScene.appendChild(marker);
+            var toy = toys.filter(toy=>toy.id===marker.id)[0];
+            if(!toy.isOutOfStock) {
+                //
+            };
             var model = document.createElement('a-entity');
             model.setAttribute('id',`model-${toy.id}`);
             var {position, rotation, scale} = toy.modelGeometry;
@@ -20,6 +24,7 @@ AFRAME.registerComponent('create-markers',{
             model.setAttribute('scale',scale);
             model.setAttribute('gltf-model',`url(${toy.modelUrl})`);
             model.setAttribute('gesture-handler',{});
+            model.setAttribute('visible',false);
             marker.appendChild(model);
             var mainPlane = document.createElement('a-plane');
             mainPlane.setAttribute('id',`main-plane-${toy.id}`);
@@ -35,6 +40,7 @@ AFRAME.registerComponent('create-markers',{
             });
             mainPlane.setAttribute('width',1.7);
             mainPlane.setAttribute('height',1.5);
+            mainPlane.setAttribute('visible',false);
             marker.appendChild(mainPlane);
             var titlePlane = document.createElement('a-plane');
             titlePlane.setAttribute('id',`title-plane-${toy.id}`);
@@ -96,6 +102,43 @@ AFRAME.registerComponent('create-markers',{
                 align: 'left'
             });
             mainPlane.appendChild(description);
+            var pricePlane = document.createElement('a-image');
+            pricePlane.setAttribute('id',`price-plane-${toy.id}`);
+            pricePlane.setAttribute('src','https://raw.githubusercontent.com/whitehatjr/menu-card-app/main/black-circle.png');
+            pricePlane.setAttribute('width',0.8);
+            pricePlane.setAttribute('height',0.8);
+            pricePlane.setAttribute('position',{
+                x: -1.3,
+                y: 0,
+                z: 0.3
+            });
+            pricePlane.setAttribute('rotation',{
+                x: -90,
+                y: 0,
+                z: 0
+            });
+            pricePlane.setAttribute('visible',false);
+            marker.appendChild(pricePlane);
+            var price = document.createElement('a-entity');
+            price.setAttribute('id',`price-${toy.id}`);
+            price.setAttribute('position',{
+                x: 0.03,
+                y: 0.05,
+                z: 0.1
+            });
+            price.setAttribute('rotation',{
+                x: 0,
+                y: 0,
+                z: 0
+            });
+            price.setAttribute('text',{
+                font: 'mozillavr',
+                color: 'white',
+                width: 3,
+                align: 'center',
+                value: `only\n $${toy.price.toFixed(2)}`
+            });
+            pricePlane.appendChild(price);
         });
     },
     getToys: async function() {
--- markerHandler.js
+++ markerHandler.js
@@ -1,30 +1,107 @@
+var currentUser = null;
 AFRAME.registerComponent('marker-handler',{
     init: async function() {
-        this.el.addEventListener('markerFound',this.handleMarkerFound);
+        if(currentUser==null) {
+            this.askCurrentUser();
+        }
+        var toys = await this.getToys();
+        var markerId = this.el.id;
+        this.el.addEventListener('markerFound',()=>{
+            if(currentUser) {
+                this.handleMarkerFound(toys,markerId);
+            }
+        });
         this.el.addEventListener('markerLost',this.handleMarkerLost);
     },
-    handleMarkerFound: function() {
-        var buttonDiv = document.getElementById('button-div');
-        buttonDiv.style.display = 'flex';
-        var summaryButton = document.getElementById('summaryButton');
-        var orderButton = document.getElementById('orderButton');
-        summaryButton.addEventListener('click',()=>{
+    askCurrentUser: function() {
+        var iconUrl = "https://img.icons8.com/material-outlined/50/000000/user--v1.png";
+        swal({
+            title: 'Welcome to our toy store!',
+            icon: iconUrl,
+            content: {
+                element: 'input',
+                attributes: {
+                    placeholder: 'Please enter your user id',
+                    type: 'number',
+                    min: 1
+                }
+            },
+            closeOnClickOutside: false
+        }).then((inputValue)=>{
+            currentUser = inputValue;
+        });
+    },
+    getToys: async function() {
+        return await firebase.firestore().collection('toys').get().then(snapshot=>{
+            return snapshot.docs.map(doc=>doc.data());
+        }).catch(error=>{
+            console.log(error);
+        });
+    },
+    handleMarkerFound: function(toys,markerId) {
+        var toy = toys.filter(toy=>toy.id===markerId)[0];
+        if(toy.isOutOfStock) {
             swal({
                 icon: 'warning',
-                title: 'Summary of toy',
-                text: 'Work in progress'
+                title: toy.toyName.toUpperCase(),
+                text: 'This toy is out of stock',
+                timer: 2500,
+                buttons: false
             });
-        });
-        orderButton.addEventListener('click',()=>{
-            swal({
-                icon: 'https://i.imgur.com/4NZ6uLY.jpeg',
-                title: 'Thanks for ordering',
-                text: 'Your order will arrive soon'
+        } else {
+            var model = document.querySelector(`#model-${toy.id}`);
+            model.setAttribute('position',toy.modelGeometry.position);
+            model.setAttribute('rotation',toy.modelGeometry.rotation);
+            model.setAttribute('scale',toy.modelGeometry.scale);
+            model.setAttribute('visible',true);
+            var mainPlane = document.querySelector(`#main-plane-${toy.id}`);
+            mainPlane.setAttribute('visible',true);
+            var pricePlane = document.querySelector(`#price-plane-${toy.id}`);
+            pricePlane.setAttribute('visible',true);
+            var buttonDiv = document.getElementById('button-div');
+            buttonDiv.style.display = 'flex';
+            var summaryButton = document.getElementById('summaryButton');
+            var orderButton = document.getElementById('orderButton');
+            summaryButton.addEventListener('click',()=>{
+                swal({
+                    icon: 'warning',
+                    title: 'Summary of toy',
+                    text: toy.description
+                });
             });
-        });
+            orderButton.addEventListener('click',()=>{
+                var user;
+                currentUser<=9?user='0'+currentUser.toString():user=currentUser.toString();
+                this.handleOrder(user,toy);
+                swal({
+                    icon: 'https://i.imgur.com/4NZ6uLY.jpeg',
+                    title: 'Thanks for ordering',
+                    text: 'Your order will arrive soon'
+                });
+            });
+        }
     },
     handleMarkerLost: function() {
         var buttonDiv = document.getElementById('button-div');
         buttonDiv.style.display = 'none';
+    },
+    handleOrder: function(currentUser,toy) {
+        firebase.firestore().collection('users').doc(`U${currentUser}`).get().then((doc)=>{
+            var data = doc.data();
+            if(data['currentOrder'][toy.id]) {
+                data['currentOrder'][toy.id]['quantity']+=1;
+                var currentQuantity = data['currentOrder'][toy.id]['quantity'];
+                data['currentOrder'][toy.id]['subtotal'] = currentQuantity*toy.price;
+            } else {
+                data['currentOrder'][toy.id] = {
+                    item: toy.toyName,
+                    price: toy.price,
+                    quantity: 1,
+                    subtotal: toy.price*1
+                };
+            };
+            data.totalBill += toy.price;
+            firebase.firestore().collection('users').doc(doc.id).update(data);
+        });
     }
 });
